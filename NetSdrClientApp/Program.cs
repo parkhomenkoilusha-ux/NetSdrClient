bba39ff74ad4a2a789517bcdb63ceada7ef50e35
Olga <olga@example.com>
2025-12-08 03:07:23 +0200

    Final version

--- NetSdrClientApp/Program.cs
+++ NetSdrClientApp/Program.cs
@@ -1,46 +1,60 @@
-﻿using NetSdrClientApp;
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis; // <--- Необходимая библиотека
+using NetSdrClientApp;
 using NetSdrClientApp.Networking;
 
-Console.WriteLine(@"Usage:
+namespace NetSdrClientApp
+{
+    [ExcludeFromCodeCoverage] // <--- Теперь атрибут видит класс и работает
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(@"Usage:
 C - connect
 D - disconnet
 F - set frequency
 S - Start/Stop IQ listener
 Q - quit");
 
-var tcpClient = new TcpClientWrapper("127.0.0.1", 5000);
-var udpClient = new UdpClientWrapper(60000);
+            var tcpClient = new TcpClientWrapper("127.0.0.1", 5000);
+            var udpClient = new UdpClientWrapper(60000);
 
-var netSdr = new NetSdrClient(tcpClient, udpClient);
+            var netSdr = new NetSdrClient(tcpClient, udpClient);
 
-while (true)
-{
-    var key = Console.ReadKey(intercept: true).Key;
-    if (key == ConsoleKey.C)
-    {
-        await netSdr.ConnectAsync();
-    }
-    else if (key == ConsoleKey.D)
-    {
-        netSdr.Disconect();
-    }
-    else if (key == ConsoleKey.F)
-    {
-        await netSdr.ChangeFrequencyAsync(20000000, 1);
-    }
-    else if (key == ConsoleKey.S)
-    {
-        if (netSdr.IQStarted)
-        {
-            await netSdr.StopIQAsync();
+            while (true)
+            {
+                var key = Console.ReadKey(intercept: true).Key;
+
+                if (key == ConsoleKey.C)
+                {
+                    await netSdr.ConnectAsync();
+                }
+                else if (key == ConsoleKey.D)
+                {
+                    netSdr.Disconect();
+                }
+                else if (key == ConsoleKey.F)
+                {
+                    await netSdr.ChangeFrequencyAsync(20000000, 1);
+                }
+                else if (key == ConsoleKey.S)
+                {
+                    if (netSdr.IQStarted)
+                    {
+                        await netSdr.StopIQAsync();
+                    }
+                    else
+                    {
+                        await netSdr.StartIQAsync();
+                    }
+                }
+                else if (key == ConsoleKey.Q)
+                {
+                    break;
+                }
+            }
         }
-        else
-        {
-            await netSdr.StartIQAsync();
-        }
-    }
-    else if (key == ConsoleKey.Q)
-    {
-        break;
     }
 }
